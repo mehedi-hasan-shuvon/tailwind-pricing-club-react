a7098aa0185218f793815456fdb6130b4ef3b511
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-03-28 20:23:41 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-6xl'>welcome to my pricing club</h1>
+      <Header></Header>
+
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from '../Link/Link';
+
+const Navbar = () => {
+    const routes = [
+        { id: 1, name: 'Home', link: '/home' },
+        { id: 2, name: 'Shop', link: '/shop' },
+        { id: 3, name: 'Deals', link: '/deals' },
+        { id: 4, name: 'Coupons', link: '/coupons' },
+        { id: 5, name: 'contact', link: '/contact' }
+    ]
+    return (
+        <nav>
+            <ul>
+
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
